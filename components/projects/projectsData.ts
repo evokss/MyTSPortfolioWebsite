110cada48c40c8938fbde910730dd7f0243febec
Eva <ekoss.prog@gmail.com>
2025-05-07 18:37:46 +0400

    feat(projects): update technologies field in project data

--- components/projects/projectsData.ts
+++ components/projects/projectsData.ts
@@ -1,4 +1,3 @@
-// Define the type for a project object
 export interface Project {
   id: number;
   name: string;
@@ -9,7 +8,6 @@ export interface Project {
   imageUrl: string;
 }
 
-// Define the array with explicit typing
 export const projects: Project[] = [
   {
     id: 1,
@@ -26,7 +24,7 @@ export const projects: Project[] = [
     name: "My Fav Videos",
     description:
       "A dynamic and user-friendly web application designed to curate and display a personal collection of favorite YouTube videos in an elegant grid layout. Built with React, Next.js and Tailwind CSS for a fully responsive, visually appealing design that adapts effortlessly across devices.",
-    technologies: ["React", "Tailwind", "Next.js", "HTML", "CSS"],
+    technologies: ["JavaScript" ,"React", "Next.js", "HTML", "CSS", "Tailwind"],
     githubLink: "https://github.com/evokss/MyFavVideos",
     deployLink: "https://my-fav-videos.vercel.app/",
     imageUrl: "/icons/movie.png",
@@ -36,7 +34,7 @@ export const projects: Project[] = [
     name: "React Online Market",
     description:
       "A modern e-commerce web application built with React, featuring a catalog of articles/products, user authentication, shopping basket functionality, and user profiles. The application implements a robust state management system combining Redux and custom hooks, with support for internationalization.",
-    technologies: ["React", "React Router", "Redux", "HTML", "CSS"],
+    technologies: ["JavaScript", "React", "React Router", "Redux", "HTML", "CSS"],
     githubLink: "https://github.com/evokss/ReactOnlineMarket",
     deployLink: "https://react-products-market.vercel.app/",
     imageUrl: "/icons/shop.png",
@@ -46,7 +44,7 @@ export const projects: Project[] = [
     name: "Meme Factory",
     description:
       "Generates random meme images from a preloaded dataset, and view the resulting meme on the screen. The app dynamically updates the displayed meme with each click, providing an interactive experience.",
-    technologies: ["TypeScript", "React", "Node.js", "HTML", "CSS"],
+    technologies: ["TypeScript", "React", "HTML", "CSS"],
     githubLink: "https://github.com/evokss/MemeFactory",
     deployLink: "https://memes-factory.vercel.app/",
     imageUrl: "/icons/troll-face.png",
@@ -66,7 +64,7 @@ export const projects: Project[] = [
     name: "RetroTube",
     description:
       "The project replicates the core design of YouTube's interface, featuring a structured video grid, navigation bar, and user-friendly design elements, all built without the use of JavaScript.",
-    technologies: ["Bootstrap", "HTML", "CSS"],
+    technologies: ["HTML", "CSS", "Bootstrap"],
     githubLink: "https://github.com/evokss/RetroTube.github.io",
     deployLink: "https://evokss.github.io/RetroTube.github.io/",
     imageUrl: "/icons/youtube.png",
@@ -76,7 +74,7 @@ export const projects: Project[] = [
     name: "Airbnb Experiences Mobile Clone",
     description:
       "A mobile-first app showcasing online experiences with badges for availability and location. It employs reusable components, mapping techniques, and conditional rendering for scalable, responsive design.",
-    technologies: ["React", "HTML", "CSS"],
+    technologies: ["TypeScript", "React", "HTML", "CSS"],
     githubLink:
       "https://github.com/evokss/ReactProjects/tree/main/airbnb-clone",
     deployLink: "https://my-air-clone.vercel.app/",
@@ -87,7 +85,7 @@ export const projects: Project[] = [
     name: "ReactTS Insights",
     description:
       "A mobile-first application presenting essential React and TypeScript facts in a responsive and user-friendly interface. It features a structured design with a reusable Navbar and Main components.",
-    technologies: ["React", "TypeScript", "HTML", "CSS"],
+    technologies: ["TypeScript", "React", "HTML", "CSS"],
     githubLink: "https://github.com/evokss/React-TS-Facts-Project",
     deployLink: "https://react-ts-facts-project.vercel.app/",
     imageUrl: "/icons/react.png",
